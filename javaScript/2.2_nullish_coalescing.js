dfb2953a3267bc9ad0d15ab8164da73eb2427ef7
Pushkar Dangi <pushkardangi746@gmail.com>
2023-07-05 14:11:56 +0530

    nullish coalescing added

---
+++ javaScript/2.2_nullish_coalescing.js
@@ -0,0 +1,12 @@
+// Nullish Coalescing (??)
+
+// it only addresses null and undefinded value
+// if the first value is null or undefined, take the next value
+
+console.log(" zero -",               0 ?? "HiddenKey" );
+console.log(" NaN -",              NaN ?? "HiddenKey" );
+console.log(" number -",            33 ?? "HiddenKey" );
+console.log(" false -",          false ?? "HiddenKey" );
+console.log(" empty string -",      "" ?? "HiddenKey" );
+console.log(" null -",            null ?? "HiddenKey" );
+console.log(" undefined -",  undefined ?? "HiddenKey" );
